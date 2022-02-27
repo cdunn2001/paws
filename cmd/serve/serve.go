ecd8ae498d1bb8c7a11d0960db3f2c7ee891ce39
Christopher Dunn <cdunn@pacificbiosciences.com>
2022-02-26 21:32:52 -0800

    Basic example (but not sure we need log.Fatal() at all)

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	//"fmt"
-	"log"
+	//"log" // log.Fatal()
 	// "pacb.com/seq/paws/pkg/stuff"
 	// "pacb.com/seq/paws/pkg/stiff"
 	//"github.com/gofiber/fiber/v2"
@@ -10,20 +10,19 @@ import (
 	//"github.com/gofiber/fiber/v2/utils"
 	//"github.com/gofiber/template/html"
 	"github.com/gin-gonic/gin"
+	"runtime" // only for GOOS
 )
 
 func main() {
 	router := gin.Default()
-	/*
-		router.GET("/hello", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "Hello World!",
-			})
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
 		})
+	})
 
-		router.GET("/os", func(c *gin.Context) {
-			c.String(200, runtime.GOOS)
-		})
-		log.Fatal(router.Run(":5000"))
-	*/
+	router.GET("/os", func(c *gin.Context) {
+		c.String(200, runtime.GOOS)
+	})
+	router.Run(":5000")
 }
