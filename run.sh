cd3881eea4dee4ed60e4dc1c5a6d94d5b5200087
Christopher Dunn <cdunn@pacificbiosciences.com>
2022-03-16 10:29:13 -0700

    Show headers from curl; only first call for now

--- run.sh
+++ run.sh
@@ -3,25 +3,28 @@ set -vex
 P=23633
 E=localhost:$P
 
-curl -f -X GET $E/sockets
+curl -i -f -X GET $E/sockets
 # Should print [1,2,3,4] quoted.
 
 # darkcalFileUrl=file:/data/nrta/0/darkcal.h5
-curl -f -X POST -d @sims/darkcal.start.json $E/sockets/1/darkcal/start
-#curl -X POST -d '{"calibFileUrl": ""}' $E/sockets/1/darkcal/start
-curl -f -X POST $E/sockets/1/darkcal/stop
-curl -f -X POST $E/sockets/1/darkcal/reset
+curl -i -f -X POST -d @sims/darkcal.start.json $E/sockets/1/darkcal/start
+#curl -i -X POST -d '{"calibFileUrl": ""}' $E/sockets/1/darkcal/start
 
-curl -f -X POST -d @sims/loadingcal.start.json $E/sockets/1/loadingcal/start
-curl -f -X POST $E/sockets/1/loadingcal/stop
-curl -f -X POST $E/sockets/1/loadingcal/reset
+exit 0
+# For later:
+curl -i -f -X POST $E/sockets/1/darkcal/stop
+curl -i -f -X POST $E/sockets/1/darkcal/reset
 
-curl -f -X POST -d @sims/basecaller.start.json $E/sockets/1/basecaller/start
-curl -f -X POST $E/sockets/1/basecaller/stop
-curl -f -X POST $E/sockets/1/basecaller/reset
+curl -i -f -X POST -d @sims/loadingcal.start.json $E/sockets/1/loadingcal/start
+curl -i -f -X POST $E/sockets/1/loadingcal/stop
+curl -i -f -X POST $E/sockets/1/loadingcal/reset
 
-curl -f -X GET $E/postprimaries
-curl -f -X POST -d @sims/baz2bam.start.json $E/postprimaries
-curl -f -X GET $E/postprimaries
-curl -f -X POST $E/postprimaries/m123/stop
-curl -f -X DELETE $E/postprimaries/m123
+curl -i -f -X POST -d @sims/basecaller.start.json $E/sockets/1/basecaller/start
+curl -i -f -X POST $E/sockets/1/basecaller/stop
+curl -i -f -X POST $E/sockets/1/basecaller/reset
+
+curl -i -f -X GET $E/postprimaries
+curl -i -f -X POST -d @sims/baz2bam.start.json $E/postprimaries
+curl -i -f -X GET $E/postprimaries
+curl -i -f -X POST $E/postprimaries/m123/stop
+curl -i -f -X DELETE $E/postprimaries/m123
